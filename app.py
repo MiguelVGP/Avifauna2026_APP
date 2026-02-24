6bfdc9a5f4e8c98daca9e2c19560e00f736e5611
MiguelVGP <113519848+MiguelVGP@users.noreply.github.com>
2026-02-24 10:50:33 +0000

    Fix variable name for larus fuscus image path

--- app.py
+++ app.py
@@ -839,7 +839,7 @@ elif section == "🫧 Bubble — Top espécies":
                 pass
 
             try:
-                larus_fuscus_unicolor_path = ASSETS_IMG / "larus_fuscus.jpg"   # <-- coloca aqui o ficheiro
+                larus_fuscus_path = ASSETS_IMG / "larus_fuscus.jpg"   # <-- coloca aqui o ficheiro
                 species_images["larus fuscus"] = image_to_circular_data_uri(str(larus_fuscus_path), out_px=420)
             except Exception:
                 pass
